0f2de7ed29851d7f84d4d19fc935e0cbb42f63c1
Rohit Sharma <mail2rohitsharmappn@gmail.com>
2023-06-17 13:15:47 +0530

    first commit

---
+++ src/AppClass.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+export default class AppClass extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0,
+    }
+  }
+
+  increment = () =>{
+   this.setState({
+    count: this.state.count + 1
+  })
+  }
+  decrement = () =>{
+   this.setState({
+    count: this.state.count - 1
+  })
+  };
+
+  render() {
+    return(
+    <div>
+      <h1>Welcome to my counter app</h1>
+      <p>The count is: {this.state.count}</p>
+      <button onClick={this.decrement}>-</button>
+      <button onClick={this.increment}>+</button>
+    </div>
+    );
+  }
+}
+
+
